9076824e640a32d85c915671753e06b3eb8505e3
QuickSwimming <quickswimming2022@gmail.com>
2024-07-17 22:36:13 +0700

    Update scripts.js perbaikan 113

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-// scripts.js
+// Ambil elemen-elemen yang diperlukan dari DOM
+const menuToggle = document.getElementById('menu-toggle');
+const dropdownMenu = document.getElementById('dropdown-menu');
 
-// Optionally add any custom JavaScript functionalities here if needed
+// Tambahkan event listener untuk klik pada tombol menu
+menuToggle.addEventListener('click', function() {
+    // Toggle untuk menampilkan atau menyembunyikan dropdown menu
+    if (dropdownMenu.style.display === 'block') {
+        dropdownMenu.style.display = 'none';
+    } else {
+        dropdownMenu.style.display = 'block';
+    }
+});
